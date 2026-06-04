3d3e3acb353a083bb1cbb3223ab583b7d500be1b
Dmitry Kokorin <dmitri.kokorin@gmai.com>
2026-06-04 13:53:38 -0700

    chore: bump version to 1.4.1 in functions.php and style.css

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
 use DBA\Theme\Theme_Bootstrap;
 
 if ( ! defined( 'DBA_VERSION' ) ) {
-	define( 'DBA_VERSION', '1.4.0' );
+	define( 'DBA_VERSION', '1.4.1' );
 }
 
 $dba_autoload = get_template_directory() . '/vendor/autoload.php';
